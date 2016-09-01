a54646c58277bf2ebf6ea354498f05770913adb2
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-01 12:03:18 +0300

    lab 4 done + annotations list bug fixed

--- helloWorld/src/main/java/lab/model/UsualPerson.java
+++ helloWorld/src/main/java/lab/model/UsualPerson.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import javax.persistence.*;
 import java.util.List;
 
@@ -38,6 +39,7 @@ public class UsualPerson implements Person {
     @Value("true")
     private boolean isProgrammer;
 
+    @Resource
     private List<String> contacts;
 
     @Override
