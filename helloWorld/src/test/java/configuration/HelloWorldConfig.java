c8412208c3be4ea2c43b09e4de501853c6232ef7
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-01 16:57:28 +0300

    lab 4 done + JavaConfig + properties + annotations + xml

--- helloWorld/src/test/java/configuration/HelloWorldConfig.java
+++ helloWorld/src/test/java/configuration/HelloWorldConfig.java
@@ -1,36 +1,36 @@
 package configuration;
 
-import lab.model.Country;
 import lab.model.Person;
 import lab.model.UsualPerson;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource("person.properties")
+@PropertySource("person.properties")
+@ImportResource(locations = "application-context.xml")
+@ComponentScan(basePackages = "lab.model")
 public class HelloWorldConfig {
 
-
+    @Autowired
+    Environment env;
 
     @Bean
     public Person person() {
         return new UsualPerson()
-                .setName("John Smith")
-                .setHeight(1.78f)
-                .setAge(35)
-                .setProgrammer(true);
+                .setName(env.getProperty("name"))
+                .setHeight(Float.parseFloat(env.getProperty("height")))
+                .setAge(Integer.parseInt(env.getProperty("age")))
+                .setProgrammer(env.getProperty("programmer").equalsIgnoreCase("true"));
     }
 
-    @Bean
-    public List<String> contacts() {
-        return Arrays.asList("asd@asd.ru", "+7-234-456-67-89");
-    }
+//    @Bean
+//    public List<String> contacts() {
+//        return Arrays.asList("1", "asd@asd.ru", "+7-234-456-67-89");
+//    }
 
-    @Bean
-    public Country country() {
-        return new Country(1, "Russia", "RU");
-    }
+//    @Bean
+//    public Country country() {
+//        return new Country(1, "Russia", "RU");
+//    }
 }
