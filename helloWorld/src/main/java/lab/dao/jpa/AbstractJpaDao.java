d819af71245deed8fa0bf88d3075abfad7c7daf3
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-14 17:34:32 +0300

    Lab 8 (transaction) done

--- helloWorld/src/main/java/lab/dao/jpa/AbstractJpaDao.java
+++
@@ -1,9 +0,0 @@
-package lab.dao.jpa;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-
-public class AbstractJpaDao {
-	@PersistenceUnit
-	protected EntityManagerFactory emf;
-}
--- helloWorld/src/main/java/lab/service/CountryServiceImpl.java
+++ helloWorld/src/main/java/lab/service/CountryServiceImpl.java
@@ -1,20 +1,22 @@
 package lab.service;
 
-import java.util.List;
-
 import lab.dao.CountryDao;
 import lab.model.Country;
-
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 //@Repository is more convenient declaration for such a class than general @Service
 @Repository
+@Transactional
+@Data
 public class CountryServiceImpl implements CountryService {
-
 	
+	@SuppressWarnings("SpringAutowiredFieldsWarningInspection")
 	@Autowired
 	private CountryDao countryDao;
 
@@ -37,40 +39,38 @@ public class CountryServiceImpl implements CountryService {
 		}
 	}
 
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public List<Country> getAllCountriesRequired() {
 		return countryDao.getCountryList();
 	}
 
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
 	public List<Country> getAllCountriesRequiresNew() {
 		return countryDao.getCountryList();
 	}
 
+	@Transactional(readOnly = false, propagation = Propagation.SUPPORTS)
 	public List<Country> getAllCountriesSupports() {
 		return countryDao.getCountryList();
 	}
 
+	@Transactional(readOnly = false, propagation = Propagation.NEVER)
 	public List<Country> getAllCountriesNever() {
 		return countryDao.getCountryList();
 	}
 
+	@Transactional(readOnly = false, propagation = Propagation.MANDATORY)
 	public List<Country> getAllCountriesMandatory() {
 		return countryDao.getCountryList();
 	}
 
+	@Transactional(readOnly = false, propagation = Propagation.SUPPORTS)
 	public List<Country> getAllCountriesNotSupported() {
 		return countryDao.getCountryList();
 	}
 
 	public List<Country> getAllCountries() {
 		return countryDao.getCountryList();
-	}
-
-	public CountryDao getCountryDao() {
-		return countryDao;
-	}
-
-	public void setCountryDao(CountryDao countryDao) {
-		this.countryDao = countryDao;
 	}
 
 }
